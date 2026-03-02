01b6fc5646cf98bf9b8b4f9ccb68124f36ffeb45
xavion marable <xmarable@purdue.edu>
2026-03-02 23:03:08 +0000

    small changes

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,12 +1,12 @@
 import { Header } from "@/components/Header";
 import { SignUp } from "@/components/SignUp";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
 export default async function SignUpPage() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const session = await getServerSession(authOptions) as any;
+  const session = (await getServerSession(authOptions)) as any;
   if (session) {
     redirect("/dashboard");
   }
