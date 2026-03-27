d185313bea25e96983279d5c1605f469fc697c6f
xavion marable <xmarable@purdue.edu>
2026-03-27 18:15:29 +0000

    fixed small syntax error

--- app/api/groups/[groupId]/sms/route.ts
+++ app/api/groups/[groupId]/sms/route.ts
@@ -7,48 +7,64 @@ import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
 const MessageSchema = z.object({
-    content: z.string().trim().min(1, "Message cannot be empty").max(500, "Message too long")
-})
+  content: z
+    .string()
+    .trim()
+    .min(1, "Message cannot be empty")
+    .max(500, "Message too long"),
+});
 
-export async function POST(req: NextRequest, { params }: { params: Promise<{ groupId: String }>}) {
-    const session = await getServerSession(authOptions);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const userId = (session?.user as any)?.userId as string | undefined;
-    if (!userId) {
-        return null;
-    }
+export async function POST(
+  req: NextRequest,
+  { params }: { params: Promise<{ groupId: string }> },
+) {
+  const session = await getServerSession(authOptions);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const userId = (session?.user as any)?.userId as string | undefined;
 
-    await dbConnect();
-    const user = await User.findOne({ userId: userId });
-    if (!user) {
-        return NextResponse.json({ error: "Unauthorized"}, { status: 401});
-    }
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
-    const { groupId } = await params;
-    const group = await TravelGroup.findOne({ groupID: groupId });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if (!group.membersList.some((m: any) => m.userId.toString === userId && m.role === "Leader")) {
-        return NextResponse.json({ error: "Unauthorized"}, { status: 401});
-    }
+  await dbConnect();
+  const user = await User.findOne({ userId: userId });
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
-    const body = await req.json();
-    const message = MessageSchema.safeParse(body);
-    if (!message.success) {
-        return NextResponse.json(
-            { error: "Invalid Message", details: message.error.flatten()},
-            { status: 400 }
-        );
-    }
+  const { groupId } = await params;
+  const group = await TravelGroup.findOne({ groupID: groupId });
 
-    const newSMS = {
-        topic: "yes"
-    }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    group.smsLogs.push(newSMS as any);
-    await group.save();
+  if (!group) {
+    return NextResponse.json({ error: "Group not found" }, { status: 404 });
+  }
 
-    // TODO send sms' with twilio
+  if (
+    !group.membersList.some(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (m: any) => m.userId.toString() === userId && m.role === "Leader",
+    )
+  ) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
+  const body = await req.json();
+  const message = MessageSchema.safeParse(body);
+  if (!message.success) {
+    return NextResponse.json(
+      { error: "Invalid Message", details: message.error.flatten() },
+      { status: 400 },
+    );
+  }
 
-    return NextResponse.json({ message: "Success" }, { status: 201 });
+  const newSMS = {
+    topic: "yes",
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  group.smsLogs.push(newSMS as any);
+  await group.save();
+
+  // TODO send sms' with twilio
+
+  return NextResponse.json({ message: "Success" }, { status: 201 });
 }
